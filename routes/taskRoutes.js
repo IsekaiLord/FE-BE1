35bbf602adf184ea488be1fa8d22761fc49d3105
ganzinfo <varga.tamas@h2co3.at>
2025-12-04 13:31:29 +0100

    token check in task listing

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const taskController = require('../controllers/taskController');
 const { createTaskValidationRules, paginationValidationRules } = require('../validators/taskValidators');
 const validate = require('../middlewares/validationHandler');
+const authenticateToken = require('../middlewares/authMiddleware');
 
 // POST /tasks - Új feladat létrehozása
 router.post('/', createTaskValidationRules(), validate, taskController.createTask);
 
 // GET /tasks - Összes feladat lekérdezése
-router.get('/', taskController.getAllTasks);
+router.get('/', authenticateToken, taskController.getAllTasks);
 
 // GET /tasks/page/:page - Feladatok lekérdezése lapozással (max 10)
 router.get('/page/:page', paginationValidationRules(), validate, taskController.getTasksPaginated);
